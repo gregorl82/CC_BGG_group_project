49cb8fa2128b405f8d6f0c2d38270be9e5408e9b
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-11 08:49:41 +0000

    Adds basic form to InteractiveDisplay component

---
+++ client/src/components/InteractiveDisplay.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div id="interactive-display">
+    <form v-on:submit="handleSubmit" method="post">
+      <label for=""></label>
+      <input type="text" name="">
+
+      <label for=""></label>
+      <input type="text" name="">
+
+      <button type="submit"></button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'interactive-display'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
