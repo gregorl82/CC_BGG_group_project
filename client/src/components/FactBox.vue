fa7106a57bc66fe1bc074f2b0d177edba954244b
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-11 21:43:35 +0000

    Creates FactBox component and code to return a random fact from factoid array

---
+++ client/src/components/FactBox.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div id="fact-box">
+    <h3>Did you know?</h3>
+    <p v-html="getRandomFact"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fact-box',
+  props: ['factoids'],
+  computed: {
+    getRandomFact() {
+      const length = this.factoids.length
+      const index = Math.floor(Math.random() * length)
+      return this.factoids[index]
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
