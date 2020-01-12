fb7b804189961362d6be985f3af90b6ad35797bf
Greg Shanks <greg.shanks@gmail.com>
2020-01-12 11:13:18 +0000

    Added routing, imports, components. data, mounting to come, amendments to follow

---
+++ client/src/components/MainContent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="main">
+    <title :topic="topic" />
+    <SubTopic :subTopic="sub-topic" />
+  </div>
+</template>
+
+<script>
+import Title from '@/components/Title.vue';
+import SubTopic from '@/components/SubTopic.vue';
+
+export default {
+  name: 'main-content',
+  data () {
+    return {
+
+    }
+  },
+
+  mounted() {
+
+  },
+
+  components: {
+    'topic': Topic,
+    'sub-topic': subTopic
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
