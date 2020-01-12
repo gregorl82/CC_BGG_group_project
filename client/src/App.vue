b27c615913fa8f2003a70562a8d84d11bd69ce0f
Greg Shanks <greg.shanks@gmail.com>
2020-01-12 10:54:12 +0000

    Initial commit - added imports

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from '@/components/Title.vue';
+import MainContent from '@/components/MainContent.vue';
+import SideBar from '@/components/SideBar.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
