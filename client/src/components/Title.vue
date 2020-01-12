eb6def94728bd6ba3f4635d3b21a19d647a41e7c
Greg Shanks <greg.shanks@gmail.com>
2020-01-12 13:10:17 +0000

    Made amendments, removed unnecessary code. Added topic prop.

--- client/src/components/Title.vue
+++ client/src/components/Title.vue
@@ -1,27 +1,14 @@
 <template>
   <div id="title">
-    <h1>{{ this.topic }}</h1>
+    <h1>{{ topic }}</h1>
   </div>
 </template>
 
 <script>
-import SubTopic from '@/components/SubTopic';
 
 export default {
   name: 'title',
-  data () {
-    return {
-      topic: ''
-    }
-  },
-
-  mounted() {
-
-  },
-
-  components: {
-
-  }
+  props: ['topic']
 }
 </script>
 
