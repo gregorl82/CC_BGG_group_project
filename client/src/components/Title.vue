8d74cb82b91939c0c3224a5d631a5d54b0e1e11a
Greg Shanks <greg.shanks@gmail.com>
2020-01-12 11:18:07 +0000

    Iniital commit, basic structure

---
+++ client/src/components/Title.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="title">
+    <h1>{{ this.topic }}</h1>
+  </div>
+</template>
+
+<script>
+import SubTopic from '@/components/SubTopic';
+
+export default {
+  name: 'title',
+  data () {
+    return {
+      topic: ''
+    }
+  },
+
+  mounted() {
+
+  },
+
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
