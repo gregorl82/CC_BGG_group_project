54f38bb7a2809efb401d87af5b65c41c63176982
Brendan <laotsu@hotmail.co.uk>
2020-01-12 14:13:08 +0000

    Basic navigational bar

---
+++ client/src/components/SideBar.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div id="topic-list">
+    <ul>
+      <li v-for="topic in topics" @click="handleSubmit(topic)">{{topic.topicHeading}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import eventBus from '@/main.js'
+
+export default {
+  name: 'sidebar',
+  props: ['topics'],
+  methods: {
+    handleSubmit(topic) {
+      eventBus.$emit('topic-clicked', topic)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
