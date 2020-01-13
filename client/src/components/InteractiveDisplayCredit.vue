5b2e3546ce1e08048ad60c25484e28837b3c4275
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-13 20:28:00 +0000

    Adds calculations to populate chart data and displays result message

--- client/src/components/InteractiveDisplayCredit.vue
+++ client/src/components/InteractiveDisplayCredit.vue
@@ -1,19 +1,27 @@
 <template lang="html">
   <div id="interactive-display-credit">
     <div id="submission-form">
-      <form v-on:submit="handleSubmit" method="post">
+      <h2>Credit Card Example</h2>
+      <form v-on:submit="handleSubmit">
         <label for="balance">Enter credit card balance: £ </label>
-        <input type="number" name="balance" step="0.01" required>
+        <input type="number" name="balance" step="0.01" v-model.number="balance" required>
 
         <label for="apr">Enter the card APR: </label>
-        <input type="number" name="apr" step="0.1" required>
+        <input type="number" name="apr" step="0.1" v-model.number="apr" required>
 
         <label for="repayment">Enter the monthly repayment amount: £ </label>
-        <input type="number" name="repayment" step="0.01" required>
+        <input type="number" name="repayment" step="0.01" v-model.number="repayment" required>
 
         <button type="submit">Submit</button>
       </form>
     </div>
+    <div id="credit-chart">
+      <chart
+      type="AreaChart"
+      :data="chartData"
+      :options="chartOptions"/>
+    </div>
+    <h3 v-if="resultMessage" v-html="resultMessage"></h3>
   </div>
 </template>
 
@@ -27,14 +35,39 @@ export default {
   },
   data(){
     return {
-      amount: null,
-      frequencyValue: ""
+      balance: 0,
+      apr: 0,
+      repayment: 0,
+      resultMessage: "",
+      chartData: [
+        ['Month', 'Balance'],
+        [0,     0],
+        [12,    0]
+      ],
+      chartOptions: {
+        title: 'Credit Card repayments',
+        hAxis: {title: 'Month', minValue: 0, maxValue: 12},
+        vAxis: {title: 'Balance', minValue: 0, maxValue: 1000, format: '£#.##'},
+        legend: 'none',
+        height: '600'
+      }
     }
   },
   methods: {
     handleSubmit: function(event){
       event.preventDefault();
-      return null
+      this.chartData = [['Month','Balance']]
+      let month = 0;
+      let balance = this.balance;
+      while (balance >= 0) {
+        this.chartData.push([month, balance]);
+        month++;
+        balance -= this.repayment * (1 + (this.apr/100));
+      }
+      const years = Math.floor(month / 12);
+      const months = (month - (years * 12)) % 12;
+      const message = `It will take ${years} year(s) and ${months} month(s) to pay your balance`;
+      this.resultMessage = message;
     }
   }
 }
