3128c74852bf366cfbf801b5a5aa70d25d3521f7
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-14 15:09:04 +0000

    Updates form inputs to be monthly frequency, models inputs to data

--- client/src/components/InteractiveDisplaySavings.vue
+++ client/src/components/InteractiveDisplaySavings.vue
@@ -1,16 +1,16 @@
 <template lang="html">
   <div id="interactive-display">
     <div id="submission-form">
-      <form v-on:submit="handleSubmit" method="post">
-        <label for="">£ </label>
-        <input type="number" step="0.01" placeholder="0.00" v-model.number="amount" required>
-        <label for=""> every </label>
-        <select v-model="frequencyValue">
-          <option v-for="period in this.frequencyArray" :value="period.value">{{period.text}}</option>
-        </select>
-        <label for=""> for </label>
-        <input type="number" placeholder="1" required>
-        <label> year(s) </label>
+      <form v-on:submit="handleSubmit">
+        <p>Enter an amount to save regularly and see how much your money can grow!</p>
+        <label for="amount">£ </label>
+        <input type="number" name="amount" step="0.01" min="0" placeholder="0.00" v-model.number="amount" required>
+        <label for="period"> every month for </label>
+        <input type="number" name="period" placeholder="1" min="1" v-model.number="numberOfMonths" required>
+        <label> month(s) </label>
+        <label for="interest-rate"> Annual interest rate: </label>
+        <input type="number" name="interest-rate" placeholder="0.9" step="0.1" min="0" v-model.number="interestRate" required>
+        <label>% </label>
         <button type="submit">Enter</button>
       </form>
     </div>
@@ -28,30 +28,24 @@ import { GChart } from 'vue-google-charts'
 
 export default {
   name: 'interactive-display',
-  // props: ['comparitives'],
   components: {
     'chart': GChart
   },
   data(){
     return {
-      frequencyArray: [{text: "day", value: 1}, {text: "week", value: 7}, {text: "month", value: 30}],
-      amount: null,
-      frequencyValue: "",
+      amount: 0,
+      numberOfMonths: 0,
+      interestRate: 0,
       chartData: [
-        ['Time (months)', 'Amount'],
-        [0 , 0],
-        [1 , 25],
-        [2 , 50],
-        [3 , 75],
-        [4 , 100],
-        [5 , 125],
-        [6 , 150]
+        ['Month', 'Amount'],
+        [6 , 0]
       ],
       chartOptions: {
         title: 'Savings projection',
         legend: 'none',
-        hAxis: {title: 'Time(months)', minValue: 0, maxValue: 6},
-        vAxis: {title: 'Amount (£)', minValue: 0, maxValue: 100, format: '£#.##'}
+        hAxis: {title: 'Month', minValue: 0, ticks: [0, 1, 2, 3, 4, 5, 6]},
+        vAxis: {title: 'Amount saved', minValue: 0, maxValue: 100, format: '£#.##'},
+        height: '400'
       }
     }
   },
@@ -59,12 +53,6 @@ export default {
     handleSubmit: function(event){
       event.preventDefault();
       return null
-    }
-  },
-  computed: {
-    calculatedSavings() {
-      let savings = this.amount * this.frequencyValue
-      return savings;
     }
   }
 }
