b1a210f2a3f96241a051afb381407a4e4823cdea
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-15 09:44:39 +0000

    Corrects width of SubTopic components, chart forms

--- client/src/components/SubTopic.vue
+++ client/src/components/SubTopic.vue
@@ -60,14 +60,14 @@ export default {
 
 <style lang="css" scoped>
 .accordion {
-    width: 1200px;
+    width: 1100px;
     margin-left: auto;
     margin-right: auto;
     margin-top: 20px;
     transition: 0.4s ease all;
 }
 .accordion-header {
-    width: 1200px;
+    width: 1100px;
     height: 40px;
     color: white;
     background-color: #1A2F4C;
